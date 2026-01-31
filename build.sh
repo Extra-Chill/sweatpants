0283e6fad755bc837671c20df64d767e35e0c9c0
Chris Huber <chubes4@users.noreply.github.com>
2026-01-31 12:14:51 -0500

    Add symlink for fixed artifact name (workaround for homeboy#42)

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ set -e
 cd "$(dirname "$0")"
 rm -rf dist/ build/
 pipx run build --sdist
+cd dist && ln -sf sweatpants-[0-9]*.tar.gz sweatpants-latest.tar.gz && cd ..
 echo "Built: $(ls dist/*.tar.gz)"
